8f1eec142349cc97584639d66d4b10d6889295ff
Ronaldo Dantas <rondan1991@gmail.com>
2020-07-09 17:59:54 -0300

    instalando pacotes, stringr -> str_c

--- teste1.r
+++ teste1.r
@@ -1,1 +1,22 @@
 a <- 10
+
+b <- 10
+
+c <- "Joao"
+d <- "Heitor"
+c(c,d)
+
+install.packages("stringr")
+# package 'glue' successfully unpacked and MD5 sums checked
+# package 'magrittr' successfully unpacked and MD5 sums checked
+# package 'stringi' successfully unpacked and MD5 sums checked
+# package 'stringr' successfully unpacked and MD5 sums checked
+
+library(stringr)
+
+Nome <- "Joao"
+Sobrenome <- "Silva"
+
+NomeCompleto <- str_c(Nome, " ",Sobrenome)
+# junta dois objetos
+NomeCompleto
